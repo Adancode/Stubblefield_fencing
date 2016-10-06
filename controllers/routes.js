0ee3c346dc72034f1ff44dc8da7c161ca4f399b6
Michael Desantis <multishifties@yahoo.com>
2016-10-05 19:05:01 -0500

    post route logging contact form in terminal

--- controllers/routes.js
+++ controllers/routes.js
@@ -12,36 +12,46 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // Redirect to landing route by default
 router.get('/', function(req, res, next) {
-    res.redirect('/landing');
+    res.redirect('/home');
 });
 
 // GET Landing Route
-router.get('/landing', function(req, res, next) {
+router.get('/home', function(req, res, next) {
    res.render('index');
 });
 
 // GET About Route
 router.get('/about', function(req, res, next) {
-   res.send('About Page');
+   res.render('about');
 });
 
 // GET Fences Route
 router.get('/fences', function(req, res, next) {
-   res.send('Fence Page');
+   res.render('fences');
 });
 
-/*
+
 // GET Contact Route
 router.get('/contact', function(req, res, next) {
-  // if template return res.render('login', { title: 'Log In'});
-  res.sendFile(path.join(__dirname, "../views/login.html"));
+  res.render('contact');
 });
-*/
 
-// Initial load/direct
-router.use(function(req, res) {
-    res.redirect('/landing');
+// Post Contact Route
+router.post('/contact', function(req, res, next){
+	console.log(req.body);
 });
+
+// GET Error Route
+router.get('/error', function(req, res, next) {
+  // if template return res.render('login', { title: 'Log In'});
+  res.render('error');
+});
+
+// Redirect if non-existant
+router.use(function(req, res) {
+    res.redirect('/error');
+});
+
 
 // Export Router
 module.exports = router;
